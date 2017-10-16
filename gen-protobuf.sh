aa59755cb9960ff448c5f52c5dcc174af098cd51
Kenneth Shaw <kenshaw@gmail.com>
2017-10-16 11:14:56 +0700

    Fixing kerberos package dependency issue
    A number of fixes, as itemized:
    
    - Change import paths for github.com/jcmturner/gokrb5/* -> gopkg.in/jcmturner/gokrb5.v1/*
    - Update Gopkg.lock accordingly
    - Adding line end to files missing them
    - Reorder import list on class_mappings.go
    - Fix gen-protobuf.sh script and permissions
    - Generate latest message/*.pb.go files

--- gen-protobuf.sh
+++ gen-protobuf.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
+rm -rf message avatica-tmp
+
 export AVATICA_VER="rel/avatica-1.10.0"
 
-rm -rf message
-rm -rf avatica-tmp
+mkdir -p avatica-tmp
+pushd avatica-tmp &> /dev/null
 
-git init avatica-tmp
-cd avatica-tmp
-git remote add origin https://github.com/apache/calcite-avatica/
+git init
+git remote add origin https://github.com/apache/calcite-avatica.git
 git config core.sparsecheckout true
 echo "core/src/main/protobuf/*" >> .git/info/sparse-checkout
+
 git fetch --depth=1 origin $AVATICA_VER
 git checkout FETCH_HEAD
 
-cd ..
-mkdir message
-protoc --proto_path=avatica-tmp/core/src/main/protobuf/ --go_out=import_path=message/message avatica-tmp/core/src/main/protobuf/*.proto
+popd &> /dev/null
+
+mkdir -p message
+
+protoc --proto_path=avatica-tmp/core/src/main/protobuf --go_out=import_path=message:message avatica-tmp/core/src/main/protobuf/*.proto
 
 rm -rf avatica-tmp
 
